29217b368cc4eab4358372668d79c85039cca407
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-10-27 12:35:37 +0300

    Add files via upload

---
+++ StackAndQueque/syntaxisQueue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_OperationsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputnumber = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] enqunumber = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int remove = inputnumber[1];
+            int megicnum = inputnumber[2];
+            Queue<int> resultnumber = new Queue<int>(enqunumber);
+
+
+            if (remove <=resultnumber.Count)
+            {
+                for (int i = 0; i < remove; i++)
+                {
+                    resultnumber.Dequeue();
+                }
+            }
+
+            if (resultnumber.Contains(megicnum))
+            {
+                Console.WriteLine("true");
+            }
+            else if(resultnumber.Count > 0)
+            {
+                Console.WriteLine(resultnumber.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
