7d5c63313bf6bfc00a7a2d598c5c9ca87342d4fd
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-10-28 12:31:08 +0300

    Add files via upload

---
+++ StackAndQueque/StackCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace _3._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<string> operacion = new Stack<string>(input.Split().Reverse());
+            int result = 0;
+            while (operacion.Count >1)
+            {
+                int firstdig = int.Parse(operacion.Pop());
+                string oper = operacion.Pop();
+                int seconddig = int.Parse(operacion.Pop());
+
+                if (oper=="+")
+                {
+                    operacion.Push((firstdig + seconddig).ToString());
+                }
+                else if (oper=="-")
+                {
+                    operacion.Push((firstdig - seconddig).ToString());
+                }
+            }
+
+            Console.WriteLine(operacion.Pop());
+        }
+    }
+}
