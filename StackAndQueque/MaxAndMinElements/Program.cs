3017e4118b0f21b3f4198f812c8e7c0bf373d7b5
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-10-28 12:29:18 +0300

    Add files via upload

---
+++ StackAndQueque/MaxAndMinElements/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Maximum_and_Minimum_ElementExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quest = int.Parse(Console.ReadLine());
+
+            Stack<int> element = new Stack<int>();
+
+            for (int i = 0; i < quest; i++)
+            {
+                int[] comand = Console.ReadLine().Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (comand[0] == 1)
+                {
+                    element.Push(comand[1]);
+                }
+                else if (comand[0] == 2)
+                {
+                    if (element.Count > 0)
+                    {
+                        element.Pop();
+                    }
+                }
+                else if (comand[0] == 3)
+                {
+                    if (element.Count > 0)
+                    {
+                        Console.WriteLine(element.Max());
+                    }
+                }
+                else if (comand[0] == 4)
+                {
+                    if (element.Count > 0)
+                    {
+                         Console.WriteLine(element.Min());
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", element));
+        }
+    }
+}
