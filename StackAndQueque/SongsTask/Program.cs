a53e491c92de4342a612f038f5f07aef223ad83e
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-11-09 12:37:33 +0200

    Add files via upload

---
+++ StackAndQueque/SongsTask/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Songs_Queue_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputSongs = Console.ReadLine().Split(", ");
+
+            Queue<string> allSongs = new Queue<string>(inputSongs);
+
+            while (allSongs.Count > 0)
+            {
+                string comand = Console.ReadLine();
+
+                if (comand=="Play")
+                {
+                    allSongs.Dequeue();
+                }
+                else if (comand.Contains("Add"))
+                {
+                    string song = comand.Substring(4);
+                    if (allSongs.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        allSongs.Enqueue(song);
+                    }
+                }
+                else if (comand.Contains("Show"))
+                {
+                    Console.WriteLine(string.Join(", ", allSongs));
+                }
+
+                if (allSongs.Count == 0)
+                {
+                    Console.WriteLine("No more songs!");
+                }
+            }
+        }
+    }
+}
