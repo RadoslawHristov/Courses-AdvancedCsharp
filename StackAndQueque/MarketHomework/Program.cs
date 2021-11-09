5422d66faa57d55c84daea39c43a1d1d993347c6
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-11-09 12:38:50 +0200

    Add files via upload

---
+++ StackAndQueque/MarketHomework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> names = new Queue<string>();
+            int count = 0;
+            while (input != "End")
+            {
+                if (input != "Paid")
+                {
+                   names.Enqueue(input);
+                   count++;
+                }
+                else if(input=="Paid")
+                {
+                    while (names.Count > 0)
+                    {
+                        Console.WriteLine(names.Dequeue());
+                        count--;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{count} people remaining.");
+        }
+    }
+}
