6ddae3074f61ea37fe2a945805bd9a5de6d40ea6
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-11-09 12:36:15 +0200

    Add files via upload

---
+++ StackAndQueque/EvenNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumber = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> evenNumber = new Queue<int>();
+
+            for (int i = 0; i < inputNumber.Length; i++)
+            {
+                
+                if (inputNumber[i] % 2==0 )
+                {
+                    evenNumber.Enqueue(inputNumber[i]);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ",evenNumber));
+            
+          
+        }
+    }
+}
