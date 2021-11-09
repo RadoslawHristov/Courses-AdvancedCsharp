5d8cc517aeb19721788380ef7639f0424b1d150f
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-11-09 12:35:07 +0200

    Add files via upload

---
+++ StackAndQueque/fashionButique/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int CountBag = int.Parse(Console.ReadLine());
+            Stack<int> clothnes = new Stack<int>(intput);
+            int sum = 0;
+            int counts = 1;
+            while (clothnes.Count > 0)
+            {
+                
+                sum += clothnes.Peek();
+                if (sum <=CountBag)
+                {
+                    clothnes.Pop();
+                }
+                else
+                {
+                    counts++;
+                    sum = 0;
+                }
+            }
+
+            Console.WriteLine(counts);
+        }
+    }
+}
