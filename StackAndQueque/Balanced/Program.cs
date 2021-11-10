304e58d57a98daa74a431e853d45f0e092d53947
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-11-10 12:25:02 +0200

    Add files via upload

---
+++ StackAndQueque/Balanced/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Balanced_Parenthesis_Exersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> bracket = new Stack<char>();
+            bool isBalanced = true;
+            foreach (var ch in input)
+            {
+                if (ch == '[' || ch == '{' || ch == '(')
+                {
+                    bracket.Push(ch);
+                }
+
+                else if (bracket.Count ==0)
+                {
+                    isBalanced = false;
+                    break;
+                }
+                else if (ch == ']' && bracket.Peek()=='[')
+                {
+                    bracket.Pop();
+                }
+                else if (ch == '}' && bracket.Peek() == '{')
+                {
+                    bracket.Pop();
+                }
+                else if (ch == ')' && bracket.Peek() == '(')
+                {
+                    bracket.Pop();
+                }
+                else
+                {
+                    isBalanced = false;
+                }
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
