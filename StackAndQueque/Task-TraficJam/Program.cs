8548fa3410718c85cfc7d5a933a7d1195bc33e10
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-11-10 12:26:39 +0200

    Add files via upload

---
+++ StackAndQueque/Task-TraficJam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfgreenlight = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            Queue<string> cars = new Queue<string>();
+            int count = 0;
+            while (input !="end")
+            {
+                if (input=="green")
+                {
+                    for (int i = 0; i < countOfgreenlight; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            count++;
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
