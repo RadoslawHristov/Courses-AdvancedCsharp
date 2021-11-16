8e7d05a98d15b7fc93ccd73a5abb509f6117cb78
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-11-16 12:34:57 +0200

    Add files via upload

---
+++ Multidimensional Arrays/MatrixDiagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+            int sumDiag = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                    if (row==col)
+                    {
+                        sumDiag += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(sumDiag);
+
+        }
+    }
+}
