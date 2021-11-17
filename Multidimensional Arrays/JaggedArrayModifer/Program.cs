b1628c075fc661ad90b3b996ac7985d74cf1d691
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-11-17 12:57:24 +0200

    Add files via upload

---
+++ Multidimensional Arrays/JaggedArrayModifer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int[][] jugged = new int[rows][];
+
+            for (int row = 0; row < jugged.Length; row++)
+            {
+                int[] input = Console.ReadLine().Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                jugged[row] = input;
+            }
+
+            string comand = Console.ReadLine();
+
+            while (comand != "END")
+            {
+                string[] information = comand.Split();
+                if (information[0] == "Add")
+                {
+                    int row = int.Parse(information[1]);
+                    int col = int.Parse(information[2]);
+                    int value = int.Parse(information[3]);
+                    if (row < 0 || row > jugged.Length-1 || col < 0 || col > jugged[row].Length)
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                    else
+                    {
+                        jugged[row][col] += value;
+                        
+                    }
+                }
+                else if (information[0] == "Subtract")
+                {
+                    int row = int.Parse(information[1]);
+                    int col = int.Parse(information[2]);
+                    int value = int.Parse(information[3]);
+                    if (row < 0 || row > jugged.Length-1 || col < 0 || col >= jugged[row].Length)
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                    else
+                    {
+                        jugged[row][col] -= value;
+                    }
+                }
+                comand = Console.ReadLine();
+            }
+            for (int i = 0; i < jugged.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ", jugged[i]));
+            }
+        }
+    }
+}
