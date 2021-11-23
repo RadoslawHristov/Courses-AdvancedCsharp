36fdda9bcfd2f1dfbcdcbec5507a96998f12e378
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-11-23 12:41:16 +0200

    Add files via upload

---
+++ IteratorsAndComparators/frogyGame/Lake.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake<T>: IEnumerable<T>
+    {
+        private List<T> stones;
+
+        public Lake(params T[] data)
+        {
+            stones = new List<T>(data);
+        }
+
+        public void Add(T[] data)
+        {
+            foreach (var item in data)
+            {
+                stones.Add(item);
+            }
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Count; i++)
+            {
+                if (i % 2==0)
+                {
+                    yield return stones[i];
+                }
+            }
+
+            for (int i = stones.Count - 1; i >= 0; i--)
+            {
+                if (i % 2==1)
+                {
+                    yield return stones[i];
+                }
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    }
+}
---
+++ IteratorsAndComparators/frogyGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Lake<int> com = new Lake<int>();
+            com.Add(input);
+
+            Console.WriteLine(string.Join(", ",com));
+
+            //foreach (var item in com)
+            //{
+            //    Console.Write(item+","+" ");
+            //}
+        }
+    }
+}
