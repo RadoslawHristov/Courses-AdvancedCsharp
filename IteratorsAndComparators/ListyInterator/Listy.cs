1c7db09686230cb7ce5f0b029661966215983776
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-11-25 12:36:07 +0200

    Add files via upload

---
+++ IteratorsAndComparators/ListyInterator/Listy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listy
+{
+    public class ListyIterator<T>
+    {
+
+        private List<T> listy;
+
+        private int curentindex;
+
+        public ListyIterator(params T[] data)
+        {
+            listy = new List<T>(data);
+            curentindex = 0;
+        }
+
+        public bool HasNext() => curentindex < listy.Count - 1;
+
+        public void Print()
+        {
+            if (listy.Count==0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine($"{listy[curentindex]}");
+            }
+        }
+
+        public bool Move()
+        {
+            bool moves = HasNext();
+            if (moves)
+            {
+                curentindex++;
+            }
+            return moves;
+        }
+
+    }
+}
---
+++ IteratorsAndComparators/ListyInterator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Listy
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            ListyIterator<string> comand = null;
+            while (input != "END")
+            {
+                var splited = input.Split();
+                if (splited[0] == "Print")
+                {
+                    comand.Print();
+                }
+
+                else if (splited[0] == "Move")
+                {
+                    Console.WriteLine(comand.Move());
+                }
+                else if (splited[0] == "HasNext")
+                {
+                    Console.WriteLine(comand.HasNext());
+                }
+                else if (splited[0] == "Create")
+                {
+                    comand = new ListyIterator<string>(splited.Skip(1).ToArray());
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
