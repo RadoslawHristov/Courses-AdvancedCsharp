e598ec8311ca9f05a34bdd088fef6993bbd50269
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-11-29 12:51:12 +0200

    Add files via upload

---
+++ IteratorsAndComparators/customStack/CustomSatck.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    class CustomSatck<T> : IEnumerable<T>
+    {
+        private List<T> stack;
+
+        public CustomSatck(params T[] data)
+        {
+            stack = new List<T>();
+        }
+
+        public void Push(params T[] element)
+        {
+            foreach (var item in element)
+            {
+                stack.Insert(stack.Count, item);
+            }
+        }
+
+        public T Pop()
+        {
+            if (stack.Count == 0)
+            {
+                throw new ArgumentException("No elements");
+            }
+
+            T elements = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+            return elements;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = stack.Count - 1; i >= 0; i--)
+            {
+                yield return stack[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ IteratorsAndComparators/customStack/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            CustomSatck<string> staks = new CustomSatck<string>();
+
+            while (input != "END")
+            {
+                string[] splited = input.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                if (splited[0]=="Push")
+                {
+                   
+                    staks.Push(splited.Skip(1).Select(x=>x.Split(",").First()).ToArray());
+                }
+                else if (splited[0]=="Pop")
+                {
+                    try
+                    {
+                        staks.Pop();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                      
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in staks)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var sql in staks)
+            {
+                Console.WriteLine(sql);
+            }
+        }
+    }
+}
