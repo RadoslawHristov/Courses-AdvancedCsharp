0ab13676d0101c0bf91a70ce27817b51134c775c
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-01 12:30:50 +0200

    Add files via upload

---
+++ Generic/BoxOfT/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private int count;
+
+        private List<T> box = new List<T>();
+
+        public int Count
+            => box.Count;
+
+        public void Add(T element)
+        {
+            box.Add(element);
+        }
+
+        public T Remove()
+        {
+            T removeElement = this.box.Last();
+            this.box.RemoveAt(this.box.Count - 1);
+            return removeElement;
+        }
+    }
+}
