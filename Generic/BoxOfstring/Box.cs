b3a13761d69964b300f8bd2939e41d84993591a9
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-02 12:38:28 +0200

    Add files via upload

---
+++ Generic/BoxOfstring/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxofString
+{
+    public class Box<T>
+    {
+        public T Boxes  { get; set; }
+
+        public Box(T box)
+        {
+            Boxes = box;
+        }
+
+        public override  string ToString()
+        {
+            return $"{Boxes.GetType()}: {Boxes}";
+        }
+    }
+}
---
+++ Generic/BoxOfstring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericBoxofString
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                var boxes = new Box<int>(input);
+                Console.WriteLine(boxes.ToString());
+            }
+
+            // Task 1-2
+        }
+    }
+}
