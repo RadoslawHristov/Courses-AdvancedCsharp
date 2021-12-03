a2295c1457a18e82f34436ae3f0b58b44f5b4d7d
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-03 12:34:44 +0200

    Add files via upload

---
+++ Generic/GenericScale/EqualityScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    where T: struct
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T lEft,T rIght)
+        {
+            this.left = lEft;
+            this.right = rIght;
+        }
+
+        public static bool AreEqual(T t1,T t2)
+        {
+            bool result = t1.Equals(t2);
+            return result;
+        }
+    }
+}
