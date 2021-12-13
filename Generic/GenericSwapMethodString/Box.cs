7b18b4a2d18b1fff82b15b671154811d5fa29b85
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-13 12:40:04 +0200

    Add files via upload

---
+++ Generic/GenericSwapMethodString/Box.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodString
+{
+    public class Box<T>
+        where T: IComparable<T>
+    {
+        private List<T> Boxes = new List<T>();
+
+        public void AddElement(T element)
+        {
+            Boxes.Add(element);
+        }
+        public void SwapElement(int first, int second)
+        {
+            T item = this.Boxes[first];//0
+            this.Boxes[first] = this.Boxes[second];//index 0= second
+            this.Boxes[second] = item;// second=item(first)
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in Boxes)
+            {
+                sb.AppendLine($"{item.GetType()}: {item.ToString()}");
+            }
+
+            return sb.ToString();
+        }
+
+        public int GetCount(T element)
+        {
+            int count = 0;
+
+            foreach (var item in Boxes)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+
+
+    }
+}
---
+++ Generic/GenericSwapMethodString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            var element = new Box<double>();
+
+            for (int i = 0; i < count; i++)
+            {
+                double input =double.Parse(Console.ReadLine());
+
+                element.AddElement(input);
+            }
+
+            double valuesElement = double.Parse(Console.ReadLine());
+            Console.WriteLine(element.GetCount(valuesElement));
+
+            // 03. Generic Swap Method String
+            // 04. Generic Swap Method Integer
+            // 05. Generic Count Method String
+            // 06. Generic Count Method Double
+
+
+        }
+    }
+}
