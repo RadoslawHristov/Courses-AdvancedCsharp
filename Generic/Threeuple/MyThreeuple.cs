69fa577b46ef3520b96796030ca8da3118410111
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-13 12:41:26 +0200

    Add files via upload

---
+++ Generic/Threeuple/MyThreeuple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threeuple
+{
+    public class MyThreeuple<Item1, Item2,Item3>
+    {
+        public MyThreeuple(Item1 first,Item2 second,Item3 three)
+        {
+            FirstItem = first;
+            SecondItem = second;
+            ThreeItem = three;
+        }
+
+        public Item1 FirstItem { get; set; }
+
+        public Item2 SecondItem { get; set; }
+
+        public Item3 ThreeItem { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem} -> {ThreeItem}";
+        }
+    }
+}
---
+++ Generic/Threeuple/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Threeuple
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            string[] nameAndTown = Console.ReadLine().Split(" ",4);
+            string firstname = nameAndTown[0];
+            string lastname = nameAndTown[1];
+            string adress = nameAndTown[2];
+            string town = nameAndTown[3];
+            string allname = $"{firstname} {lastname}";
+            MyThreeuple<string, string, string> TownName = new MyThreeuple<string, string, string>(allname, adress, town);
+
+            string[] beerNameTrorfol = Console.ReadLine().Split();
+            string name = beerNameTrorfol[0];
+            int beer = int.Parse(beerNameTrorfol[1]);
+            bool istrue=beerNameTrorfol[2]=="drunk";
+            string isTrue = "";
+            if (istrue)
+            {
+                isTrue = "True";
+            }
+            else
+            {
+                isTrue = "False";
+            }
+            MyThreeuple<string, int, string> BeerName = new MyThreeuple<string, int, string>(name, beer,isTrue );
+
+            string[] bankAcount = Console.ReadLine().Split(" ",4);
+            string names = bankAcount[0];
+            double acount = double.Parse(bankAcount[1]);
+            string bankname = bankAcount[2];
+            MyThreeuple<string, double, string> Nameacount =
+                new MyThreeuple<string, double, string>(names, acount, bankname);
+
+            Console.WriteLine(TownName.ToString());
+            Console.WriteLine(BeerName.ToString());
+            Console.WriteLine(Nameacount.ToString());
+        }
+    }
+}
