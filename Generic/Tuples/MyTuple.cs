23bf1695acf0125c8f1d45803c6c0024b1ea51e8
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-13 12:43:01 +0200

    Add files via upload

---
+++ Generic/Tuples/MyTuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuples
+{
+    class MyTuple<item1, item2>
+    {
+        public MyTuple(item1 firstitem, item2 seconditem2)
+        {
+            FirstItem = firstitem;
+            SecondItem2 = seconditem2;
+        }
+
+        public item1 FirstItem { get; set; }
+
+        public item2 SecondItem2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem2}";
+        }
+    }
+}
---
+++ Generic/Tuples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tuples
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] nameTown = Console.ReadLine().Split(" ");
+            MyTuple<string, string> NamesAndTown = new MyTuple<string, string>($"{nameTown[0]} {nameTown[1]}", nameTown[2]);
+
+            string[] nameBeer = Console.ReadLine().Split(" ", 2);
+            string name = nameBeer[0];
+            int beer = int.Parse(nameBeer[1]);
+            MyTuple<string, int> NameandBeer = new MyTuple<string, int>(name, beer);
+
+
+            string[] number = Console.ReadLine().Split(" ",2);
+            int numInt = int.Parse(number[0]);
+            double numDouble = double.Parse(number[1]);
+            MyTuple<int, double> numbers = new MyTuple<int, double>(numInt, numDouble);
+
+            Console.WriteLine(NamesAndTown);
+            Console.WriteLine(NameandBeer);
+            Console.WriteLine(numbers);
+        }
+    }
+}
