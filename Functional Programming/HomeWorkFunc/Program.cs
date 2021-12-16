a6d11498108a1b57572a32cae240db6c0e0a7d36
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-16 12:41:28 +0200

    Add files via upload

---
+++ Functional Programming/HomeWorkFunc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Knights_of_Honor_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> name = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            Action<string> dropSir = x => Console.WriteLine("Sir"+ " " + x);
+            name.ForEach(dropSir);
+        }
+    }
+}
