0d545f5d95a2defdc905ff44a82ae6acc77c4576
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-16 12:40:07 +0200

    Add files via upload

---
+++ Functional Programming/ActionPrintFunc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Action_Print_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Action<string> output = x=>Console.WriteLine(x);
+            names.ForEach(output);
+        }
+    }
+}
