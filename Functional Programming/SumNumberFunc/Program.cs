f6a8d653807bd112d222bbb735034acf4870d5c0
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-16 12:43:12 +0200

    Add files via upload

---
+++ Functional Programming/SumNumberFunc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(number.Count());
+            Console.WriteLine(number.Sum());
+        }
+    }
+}
