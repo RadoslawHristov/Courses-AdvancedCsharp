69a1102083719b14b20ae6c373f4760b8ca88ccc
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-20 12:32:21 +0200

    Add files via upload

---
+++ Functional Programming/CoiuntUperrCaseFunc/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> upper = n => n[0] == n.ToUpper()[0];
+            string[] upperCaseWord = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(w=>upper(w))
+                .ToArray();
+               
+
+            Console.WriteLine(string.Join(Environment.NewLine,upperCaseWord));
+        }
+    }
+}
