4e47bcd36ec589323ca3d2a46a4d9a3166e2ea70
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-20 12:36:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vats = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => x * 1.2)
+                .ToArray();
+
+            foreach (var item in vats)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+    }
+}
