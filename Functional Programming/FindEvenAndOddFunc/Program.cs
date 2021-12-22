fc90b0f36b5624709911a52cd7a6591cfe0c4c6c
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2021-12-22 12:45:36 +0200

    Add files via upload

---
+++ Functional Programming/FindEvenAndOddFunc/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds_exersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> allnumber = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int first = allnumber[0];
+            int second = allnumber[1];
+
+            string comand = Console.ReadLine();
+
+            List<int> contain = new List<int>();
+
+            Predicate<int> evens = x=>
+            {
+                return x % 2 == 0;
+            };
+            Predicate<int> odd = x =>
+            {
+                return x % 2 != 0;
+            };
+            List<int> result = new List<int>();
+
+            for (int i = first; i <=second ; i++)
+            {
+                contain.Add(i);
+            }
+
+            if (comand=="even")
+            {
+                result = contain.FindAll(evens);
+            }
+            else if (comand=="odd")
+            {
+                result = contain.FindAll(odd);
+            }
+
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
