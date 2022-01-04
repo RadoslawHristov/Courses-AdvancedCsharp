d334d5a0ccc2a18b16143a511f47ea43b19b8637
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-04 12:42:14 +0200

    Add files via upload

---
+++ Functional Programming/Predicate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Predicate_For_Names_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Func<string, bool> resultNames = z => z.Length <= count;
+
+            var filteredNames = names.Where(resultNames);
+
+            Console.WriteLine(string.Join(Environment.NewLine,filteredNames));
+        }
+    }
+}
