1f7617c83331f43737a51db593058b902bb6d59f
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-04 12:40:33 +0200

    Add files via upload

---
+++ Functional Programming/ReverseAndExudeFunc/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude_Exersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+              
+            int numForDivide = int.Parse(Console.ReadLine());
+
+            Func<int, bool> results = x => x % numForDivide != 0;
+            
+            var Result = numbers.Reverse().Where(results);
+
+            Console.WriteLine(string.Join(" ",Result));
+        }
+    }
+}
