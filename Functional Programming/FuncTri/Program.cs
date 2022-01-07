12b94d802e8df1cd15f5b5251686795426efaca5
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-07 12:45:42 +0200

    Add files via upload

---
+++ Functional Programming/FuncTri/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._TriFunction_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<string, int, bool> results = (names, ascisum) => names.Sum(x => x) >= sum;
+            Func<List<string>, string> oneNames = (allnames) =>
+                allnames.FirstOrDefault(x=> results(x, sum));
+
+            Console.WriteLine(oneNames(names));
+        }
+    }
+}
