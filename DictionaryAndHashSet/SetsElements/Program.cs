6eb8fbd5160cc505ab7418a629550762867415d1
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-09 22:34:29 +0200

    Add files via upload

---
+++ DictionaryAndHashSet/SetsElements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split()
+                .Select(int.Parse)
+                .ToArray();
+            int n = input[0];
+            int m = input[1];
+            HashSet<int> numberN = new HashSet<int>();
+            HashSet<int> numberM = new HashSet<int>();
+            int sum = n + m;
+            for (int i = 0; i < sum ; i++)
+            {
+                int secInput=int.Parse(Console.ReadLine());
+                if (i < n)
+                {
+                    numberN.Add(secInput);
+                }
+                else
+                {
+                    numberM.Add(secInput);
+                }
+
+               
+            }
+
+            foreach (var item in numberN)
+            {
+                if (numberM.Contains(item))
+                {
+                     Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
