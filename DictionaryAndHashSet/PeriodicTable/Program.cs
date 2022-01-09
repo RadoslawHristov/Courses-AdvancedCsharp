8d1a5587e12ff4df6f88248ee753e8374b814b95
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-09 22:36:17 +0200

    Add files via upload

---
+++ DictionaryAndHashSet/PeriodicTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            SortedSet<string> result = new SortedSet<string>();
+
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                foreach (var item in input)
+                {
+                     result.Add(item);
+                }
+            }
+
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+        }
+        
+    }
+}
