ff16db39375ec9af6d26cf77a82fea8d1cd019d7
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-11 12:53:24 +0200

    Add files via upload

---
+++ DictionaryAndHashSet/CountOfSimbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Count_Symbols_Exersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> simbol = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+                if (!simbol.ContainsKey(ch))
+                {
+                    simbol.Add(ch,0);
+                }
+
+                simbol[ch] += 1;
+            }
+
+            foreach (var item in simbol.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
