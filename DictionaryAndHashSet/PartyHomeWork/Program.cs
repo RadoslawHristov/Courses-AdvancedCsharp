af1a7c3a7929d3e9d78102a08b0e82b1575614e9
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-12 20:56:37 +0200

    Add files via upload

---
+++ DictionaryAndHashSet/PartyHomeWork/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._SoftUni_Party_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> VIP = new HashSet<string>();
+            HashSet<string> regular = new HashSet<string>();
+
+            string input = Console.ReadLine();
+            while (input != "PARTY")
+            {
+                if (input.Length == 8)
+                {
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        char ch = input[i];
+                        if (i==0 && char.IsDigit(ch)==true)
+                        {
+                            VIP.Add(input);
+                            break;
+                        }
+                        else
+                        {
+                            regular.Add(input);
+                            break;
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+            while (input !="END")
+            {
+                if (VIP.Contains(input))
+                {
+                    VIP.Remove(input);
+                }
+                else if (regular.Contains(input))
+                {
+                    regular.Remove(input);
+                }
+                input = Console.ReadLine();
+            }
+
+            int count = VIP.Count + regular.Count;
+            Console.WriteLine(count);
+            foreach (var item in VIP)
+            {
+                Console.WriteLine(item);
+            }
+
+          
+            foreach (var itm in regular)
+            {
+                Console.WriteLine(itm);
+            }
+        }
+    }
+}
