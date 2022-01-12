d1f48e90759b603b1c1aefd54197e8d3bb0776f1
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-12 20:51:24 +0200

    Add files via upload

---
+++ DictionaryAndHashSet/RecordUniqueName/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Record_Unique_Names_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            HashSet<string> name = new HashSet<string>();
+
+
+            for (int i = 0; i < count; i++)
+            {
+                string names = Console.ReadLine();
+                name.Add(names);
+            }
+
+            foreach (var item in name)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
