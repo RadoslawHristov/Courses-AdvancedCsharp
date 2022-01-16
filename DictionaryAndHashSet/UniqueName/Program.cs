1c6c7f9b93518de5a914f3d22159aca37e47832f
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-16 18:46:41 +0200

    Add files via upload

---
+++ DictionaryAndHashSet/UniqueName/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unique_Usernames_Exersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countName = int.Parse(Console.ReadLine());
+            HashSet<string> userNames = new HashSet<string>();
+
+            for (int i = 0; i < countName; i++)
+            {
+                string username = Console.ReadLine();
+                userNames.Add(username);
+            }
+
+            foreach (var item in userNames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
