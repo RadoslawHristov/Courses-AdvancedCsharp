918ef651408293a2a9477d593937829030636f00
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-17 12:36:19 +0200

    Add files via upload

---
+++ TaskOfClass/HomeworkCreatClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CarManufacturer;
+
+namespace _1_Car_Lab
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        { 
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
