d00588cc7972c78404945f1566f10ed98eb0609c
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-18 12:44:27 +0200

    Add files via upload

---
+++ DefineClass/CreateClass/CarManifuction/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        public int Year { get; set; }
+        public double Pressure { get; set; }
+
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+    }
+    public class Engine
+    {
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+    }
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumation { get; set; }
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumation)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumation = fuelConsumation;
+        }
+
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumation, Engine engine, Tire[] tires)
+        : this(make, model, year, fuelQuantity, fuelConsumation)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+    }
+}
