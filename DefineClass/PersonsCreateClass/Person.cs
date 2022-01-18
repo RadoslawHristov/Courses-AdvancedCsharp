e32c55e3eadfa76fdea6b32847545382ff19fc2c
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-18 12:48:19 +0200

    Add files via upload

---
+++ DefineClass/PersonsCreateClass/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+
+        private string name;
+        private int age;
+
+        public Person(string name,int age)
+        {
+            this.Name = "No Name";
+            this.Age = 1;
+        }
+      
+
+
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+    }
+}
