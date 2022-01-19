bc9fa2df652aff475df7a3c3b7e4089a5e111b63
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-19 12:45:13 +0200

    Add files via upload

---
+++ DefineClass/DateModifer/DateModifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public int DaysCalculateTwoDate(string firstdate, string secontdate)
+        {
+            DateTime dateOne=DateTime.Parse(firstdate);
+            DateTime twoDate=DateTime.Parse(secontdate);
+
+            int day =(dateOne - twoDate).Days;
+            return day;
+        }
+
+
+    }
+}
---
+++ DefineClass/DateModifer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            DateModifier date = new DateModifier();
+            int result=date.DaysCalculateTwoDate(firstDate, secondDate);
+            Console.WriteLine(Math.Abs(result));
+        }
+    }
+}
