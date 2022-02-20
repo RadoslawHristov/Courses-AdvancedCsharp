33bc53def59cb2f0cacfb37e59fbc0baf7b82060
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-02-20 20:19:36 +0200

    Add files via upload

---
+++ StreamFileAndZipFile/MargeFile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader readar = new StreamReader("../../../../FileOne.txt"))
+            using (StreamReader readar2 = new StreamReader("../../../../FileTwo.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../../Output.txt"))
+                {
+                    string line = readar.ReadLine();
+                    string twoLine = readar2.ReadLine();
+                    while (line != null && twoLine !=null)
+                    {
+                        writer.WriteLine(line);
+                            line = readar.ReadLine();
+                            writer.WriteLine(twoLine);
+                            twoLine = readar2.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
