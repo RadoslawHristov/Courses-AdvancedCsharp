e4ca787eda3993deedb110a635ef23253ff872a0
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-02-21 12:41:56 +0200

    Add files via upload

---
+++ StreamFileAndZipFile/EvenLine/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Even_Line_Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Odd lines - ");
+            var resultedOddLines = new StringBuilder();
+            var file = "../../ReadFile.txt";
+
+            using (var reader = new StreamReader(file))
+            {
+                var lineNumber = 0;
+                var line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    if (lineNumber % 2 == 1)
+                    {
+                        resultedOddLines.Append($"{line}{Environment.NewLine}");
+                    }
+
+                    lineNumber++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
