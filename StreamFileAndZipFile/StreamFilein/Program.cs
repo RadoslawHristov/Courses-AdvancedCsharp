7cc21cef57f49a95060a2f813cd992df1b111028
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-02-23 12:58:09 +0200

    Add files via upload

---
+++ StreamFileAndZipFile/StreamFilein/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace StreamsFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+
+                string input = reader.ReadLine();
+                int indexofLine = 1;
+
+                while (input != null)
+                {
+                    Console.WriteLine($"{indexofLine}. {input}");
+                    indexofLine++;
+                    input = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
