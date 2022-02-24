1f9631952e1a9761e3e0921b37d95db528ac8d8d
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-02-24 12:35:51 +0200

    Add files via upload

---
+++ StreamFileAndZipFile/ZipandExtract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Zip_and_Extract_Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var zipFile = @"..\..\..\myzip.zip";
+            var file = "copyMe.png";
+
+            using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(file, Path.GetFileName(file));
+            }
+        }
+    }
+}
