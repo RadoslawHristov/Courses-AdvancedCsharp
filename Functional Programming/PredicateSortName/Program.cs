85edf882c414cf5e8ba0426b8bff21767be1a9a5
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-02-24 12:39:29 +0200

    Add files via upload

---
+++ Functional Programming/PredicateSortName/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Predicate_For_Names_Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenghtOfstring = int.Parse(Console.ReadLine());
+
+            List<string> names = new List<string>();
+
+            string[] name = Console.ReadLine()
+                .Split();
+
+            Func<string, bool> namesLenght = x => x.Length <= lenghtOfstring;
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (namesLenght(name[i]))
+                {
+                    names.Add(name[i]);
+                }
+            }
+
+            Console.Write(String.Join(Environment.NewLine, names));
+
+        }
+    }
+}
