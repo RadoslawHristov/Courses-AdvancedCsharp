dd0bcbfeace76fbc3041d1d006e5a944c9f6e020
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-02 12:29:32 +0200

    Add files via upload

---
+++ DefineClass/TwoDateMod/Date Modifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TwoDateMod
+{
+    public class Date_Modifier
+    {
+        public DateTime DateTimeOne { get; set; }
+        public DateTime DateTimeTwo { get; set; }
+
+
+        public Date_Modifier(DateTime date1,DateTime date2)
+        {
+            this.DateTimeOne = date1;
+            this.DateTimeTwo = date2;
+        }
+
+        public int GetDayForTwoDate()
+        {
+            var result = Math.Abs((DateTimeOne - DateTimeTwo).Days);
+            return result;
+        }
+    }
+}
---
+++ DefineClass/TwoDateMod/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TwoDateMod
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string dateOne = Console.ReadLine();
+            string dateTwo = Console.ReadLine();
+
+            DateTime date1 = DateTime.Parse(dateOne);
+            DateTime date2 = DateTime.Parse(dateTwo);
+
+            Date_Modifier data = new Date_Modifier(date1,date2);
+
+            Console.WriteLine(data.GetDayForTwoDate());
+            
+        }
+    }
+}
