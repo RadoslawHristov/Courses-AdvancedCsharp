30d62884b002b1749373f8c8472ab5d72264dace
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-02 12:27:59 +0200

    Add files via upload

---
+++ DefineClass/deffineClassFamily/Family.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+
+        }
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+
+        public void GetOldestMember()
+        {
+            foreach (var item in People.OrderBy(x=>x.Name))
+            {
+                if (item.Age > 30)
+                {
+                    Console.WriteLine( $"{item.Name} - {item.Age}");
+                }
+            }
+        }
+    }
+}
---
+++ DefineClass/deffineClassFamily/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Family one = new Family();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] nameAndAge = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = nameAndAge[0];
+                int age = int.Parse(nameAndAge[1]);
+
+                Person person = new Person(name, age);
+
+                one.AddMember(person);
+            }
+
+            one.GetOldestMember();
+           
+        }
+    }
+}
