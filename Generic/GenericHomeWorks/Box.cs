dc438b831b0f751bc6cc035d1f6ada4be8eb76fa
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-05 21:51:30 +0200

    Add files via upload

---
+++ Generic/GenericHomeWorks/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> element;
+
+        public Box()
+        {
+            Element = new List<T>();
+        }
+
+        public List<T> Element
+        {
+            get { return element; }
+            set { element = value; }
+        }
+
+        public int Count => Element.Count;
+
+        public void Add(T elementT)
+        {
+            this.Element.Add(elementT);
+        }
+
+        public T Remove()
+        {
+            var result = this.Element.Last();
+            this.Element.RemoveAt(Element.Count-1);
+            return result;
+        }
+    }
+}
