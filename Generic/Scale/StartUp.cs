4d25b87ce8b5ca1a9c1e2d4b4488a7fc82efa02c
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-06 19:38:51 +0200

    Add files via upload

---
+++ Generic/Scale/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            EqualityScale<int> num = new EqualityScale<int>(1, 1);
+
+            Console.WriteLine(num.AreEqual());
+            
+        }
+    }
+}
