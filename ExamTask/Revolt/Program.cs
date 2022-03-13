422bfd1e252d7b00ec26a2ea5eb0e0956fbb8054
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-13 21:21:07 +0200

    Add files via upload

---
+++ ExamTask/Revolt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02._Re_Volt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int comandcount = int.Parse(Console.ReadLine());
+
+            char[,] field = new char[matrixSize,matrixSize];
+
+            int heroRow = 0;
+            int HeroCol = 0;
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                var inputLine = Console.ReadLine();
+
+                for (int j = 0; j <field.GetLength(1); j++)
+                {
+                    field[i][j] = inputLine;
+                }       
+            }
+
+
+
+
+        }
+
+    }
+}
