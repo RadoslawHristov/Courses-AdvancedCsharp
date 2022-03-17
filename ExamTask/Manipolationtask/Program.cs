9e43318c1f0a7212e1cdec97557c8ae8127232bb
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-17 20:55:42 +0200

    Add files via upload

---
+++ ExamTask/Manipolationtask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList random = new RandomList();
+            random.Add("1");
+            random.Add("2");
+            random.Add("3");
+            random.Add("5");
+            random.Add("6");
+            random.Add("7");
+            random.Add("8");
+
+            Console.WriteLine(random.RandomString());
+            
+        }
+    }
+}
