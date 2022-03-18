5e9f19fe6f6aafb55226645bfee68cacc4354eff
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-18 12:36:08 +0200

    Add files via upload

---
+++ ExamTask/lootBox/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lootbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> firstLot = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Stack<int> secondLot = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            int itemColections = 0;
+
+
+            while (firstLot.Count > 0 && secondLot.Count > 0)
+            {
+                int curentFir = firstLot.Peek();
+                int curentSec = secondLot.Peek();
+
+                int sum = curentFir + curentSec;
+
+                if (sum % 2 == 0)
+                {
+                    itemColections += sum;
+                    firstLot.Dequeue();
+                    secondLot.Pop();
+                }
+                else
+                {
+                    firstLot.Enqueue(curentSec);
+                    secondLot.Pop();
+                }
+            }
+
+            if (firstLot.Count==0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else if (secondLot.Count==0)
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if (itemColections >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {itemColections}");
+            }
+            else if (itemColections < 100)
+            {
+                Console.WriteLine($"Your loot was poor... Value: {itemColections}");
+            }
+        }
+    }
+}
