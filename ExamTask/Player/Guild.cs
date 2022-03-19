44cd0edf2dbf9fec736db7d5384005fdcbd24e8d
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-19 19:31:24 +0200

    Add files via upload

---
+++ ExamTask/Player/Guild.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        public Guild(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Roster = new List<Player>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public List<Player> Roster { get; set; }
+        public int Count => Roster.Count;
+
+
+        public void AddPlayer(Player player)
+        {
+            if (Roster.Count < Capacity)
+            {
+                Roster.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            var curentplayer = Roster.Where(x => x.Name == name).FirstOrDefault();
+            if (curentplayer==null)
+            {
+                return false;
+            }
+
+            Roster.Remove(curentplayer);
+            return true;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            var player = Roster.Where(x => x.Name == name).First();
+            if (player.Rank != "Member")
+            {
+                player.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            var player = Roster.Where(x => x.Name == name).First();
+            if (player.Rank != "Trial")
+            {
+                player.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            Player[] player = Roster.FindAll(x=>x.Class==@class).ToArray();
+            var remove = Roster.FindAll(x => x.Class == @class);
+            Roster.RemoveAll(remove.Contains);
+            return player;
+        }
+
+        public string Report()
+        {
+            StringBuilder sv = new StringBuilder();
+            Console.WriteLine($"Players in the guild: {this.Name}");
+
+            foreach (var player in Roster)
+            {
+                sv.AppendLine(player.ToString().Trim());
+            }
+
+            return sv.ToString();
+        }
+    }
+}
---
+++ ExamTask/Player/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public Player(string name, string @class)
+        {
+            Name = name;
+            Class = @class;
+            Rank = "Trial";
+            Description = "n/a";
+        }
+
+        public Player(string name, string @class, string rank, string description)
+        {
+            Name = name;
+            Class = @class;
+            Rank = rank;
+            Description = description;
+        }
+        //• Name: string
+        //• Class: string
+        //• Rank: string – "Trial" by default
+        //• Description: string – "n/a" by default
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; }
+        public string Description { get; set; }
+
+
+        public override string ToString()
+        {
+            StringBuilder sv = new StringBuilder();
+
+            sv.AppendLine($"Player {this.Name}: {this.Class}");
+            sv.AppendLine($"Rank: {this.Rank}");
+            sv.AppendLine($"Description: {this.Description}");
+            
+            return sv.ToString();
+        }
+    }
+}
