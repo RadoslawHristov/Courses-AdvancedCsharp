7cf76ee58a69a511795456362dc946fd5eee8954
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-05-22 17:15:45 +0300

    Add files via upload

---
+++ ExamTask/Exam1304/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Meal_Plan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> dishes =
+                new Queue<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+            Stack<int> caloriesPerday =
+                new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            //salad  soup   pasta  steak
+            //350    490    680    790
+
+            string curentDish = dishes.Peek();
+            int curentCalories = caloriesPerday.Peek();
+            int count = 0;
+
+            while (dishes.Any() && caloriesPerday.Any())
+            {
+                if (curentCalories <= 0)
+                {
+                    caloriesPerday.Pop();
+                    curentCalories = caloriesPerday.Peek();
+                }
+                if (curentDish == "salad" && curentCalories >= 350)
+                {
+                    curentCalories -= 350;
+                    caloriesPerday.Pop();
+                    caloriesPerday.Push(curentCalories);
+                    dishes.Dequeue();
+                    count++;
+                    if (dishes.Count == 0)
+                    {
+                        break;
+                    }
+                    curentDish = dishes.Peek();
+                }
+                else if (curentDish == "soup" && curentCalories >= 490)
+                {
+                    curentCalories -= 490;
+                    dishes.Dequeue();
+                    caloriesPerday.Pop();
+                    caloriesPerday.Push(curentCalories);
+                    count++;
+                    if (dishes.Count == 0)
+                    {
+                        break;
+                    }
+                    curentDish = dishes.Peek();
+                }
+                else if (curentDish == "pasta" && curentCalories >= 680)
+                {
+                    curentCalories -= 680;
+                    dishes.Dequeue();
+                    caloriesPerday.Pop();
+                    caloriesPerday.Push(curentCalories);
+                    count++;
+                    if (dishes.Count == 0)
+                    {
+                        break;
+                    }
+                    curentDish = dishes.Peek();
+                }
+                else if (curentDish == "steak" && curentCalories >= 790)
+                {
+                    curentCalories -= 790;
+                    dishes.Dequeue();
+                    caloriesPerday.Pop();
+                    caloriesPerday.Push(curentCalories);
+                    count++;
+                    if (dishes.Count==0)
+                    {
+                        break;
+                    }
+                    curentDish = dishes.Peek();
+                }
+                else
+                {
+                    // 310 stack 790
+                    int oldCal = curentCalories;
+                    caloriesPerday.Pop();
+                    if (caloriesPerday.Count==0)
+                    {
+                        break;
+                    }
+                    curentCalories = caloriesPerday.Peek();
+                    int sumCalories = oldCal + curentCalories;
+                    caloriesPerday.Pop();
+                    caloriesPerday.Push(sumCalories);
+                    curentCalories = caloriesPerday.Peek();
+                }
+            }
+
+            if (dishes.Count > 0)
+            {
+                Console.WriteLine($"John ate enough, he had {count+1} meals.");
+                Console.Write("Meals left: ");
+                Console.Write("pasta.");
+            }
+            else if (caloriesPerday.Count >0)
+            {
+                Console.WriteLine($"John had {count} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ",caloriesPerday)} calories.");
+            }
+        }
+    }
+}
